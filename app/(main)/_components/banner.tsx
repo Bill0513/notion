ebec2719eadb495093d29dcec15a993153ed16d5
Keson.Cai <keson.cai@kerryprops.com>
2023-12-04 16:51:49 +0800

    banner

---
+++ app/(main)/_components/banner.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Id } from "@/convex/_generated/dataModel";
+
+interface BannerProps {
+  documentId: Id<"documents">;
+}
+
+export const Banner = ({ documentId }: BannerProps) => {
+  return <div>Banner</div>;
+};
--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -6,6 +6,7 @@ import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
 import { MenuIcon } from "lucide-react";
 import { Title } from "./title";
+import { Banner } from "./banner";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -30,17 +31,20 @@ export const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
     return null;
   }
   return (
-    <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center ga-x-4">
-      {isCollapsed && (
-        <MenuIcon
-          role="button"
-          onClick={onResetWidth}
-          className="h-6 w-6 text-muted-foreground"
-        />
-      )}
-      <div className="flex items-center justify-between w-full">
-        <Title initialData={document} />
-      </div>
-    </nav>
+    <>
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center ga-x-4">
+        {isCollapsed && (
+          <MenuIcon
+            role="button"
+            onClick={onResetWidth}
+            className="h-6 w-6 text-muted-foreground"
+          />
+        )}
+        <div className="flex items-center justify-between w-full">
+          <Title initialData={document} />
+        </div>
+      </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
+    </>
   );
 };
