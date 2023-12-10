897166f9c37ea9c6ef2605e28d33377afcb32c8e
Keson.Cai <keson.cai@kerryprops.com>
2023-12-10 19:52:25 +0800

    修改背景颜色

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import { Heroes } from "./_components/heroes";
 import { Footer } from "./_components/footer";
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
+    <div className="min-h-full flex flex-col dark:bg-[#1F1F1F]">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading></Heading>
         <Heroes></Heroes>
